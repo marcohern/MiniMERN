918a36f97b82e332e99713a8008877ae3abd1fc0
Marco Hernandez <marcohern@gmail.com>
2018-10-21 15:52:05 -0500

    passport setup and working with auth:api requests

---
+++ backend/database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new App\User();
+        $user->name = 'Marco Hernandez';
+        $user->password = Hash::make('system');
+        $user->email = 'marcohern@gmail.com';
+        $user->save();
+    }
+}
